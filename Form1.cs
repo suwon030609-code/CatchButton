0757af8d4286f2f1648102db56ad93caf52ce8ca
강준서 <suwon030609@gmail.com>
2026-03-12 17:47:37 +0900

    3단계 - 점수계산, 난이도 조절

--- Form1.cs
+++ Form1.cs
@@ -7,14 +7,26 @@ namespace CatchButton
     public partial class Form1 : Form
     {
         private readonly Random rd = new Random();
+        private int score = 0; 
 
         public Form1()
         {
             InitializeComponent();
+            UpdateGameTitle();
         }
 
+       
+        private void UpdateGameTitle()
+        {
+            this.Text = $"현재 점수: {score}점 | 위치 - X: {btnTarget.Location.X}, Y: {btnTarget.Location.Y}";
+        }
+
+  
         private void BtnTarget_MouseEnter(object sender, EventArgs e)
         {
+            score -= 10;
+
+          
             int maxX = this.ClientSize.Width - btnTarget.Width;
             int maxY = this.ClientSize.Height - btnTarget.Height;
 
@@ -23,12 +35,22 @@ namespace CatchButton
 
             btnTarget.Location = new Point(nextX, nextY);
 
-            this.Text = "버튼 위치 - X: " + btnTarget.Location.X + ", Y: " + btnTarget.Location.Y;
+           
+            UpdateGameTitle();
         }
 
+     
         private void btnTarget_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("축하합니다~!");
+          
+            score += 100;
+
+      
+            btnTarget.Width = (int)(btnTarget.Width * 0.9);
+            btnTarget.Height = (int)(btnTarget.Height * 0.9);
+
+            UpdateGameTitle();
+            MessageBox.Show("축하합니다~!", "성공");
         }
     }
 }
